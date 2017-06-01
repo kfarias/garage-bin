425b82ab3013203956b63353f5c2615d0d01fc45
Kenia Farias <11431kenia@gmail.com>
2017-06-01 12:32:59 -0600

    Adds get, post, put endpoints to server.js

--- server.js
+++ server.js
@@ -1,20 +1,96 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const md5 = require('md5')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static('public'))
+const app = express();
 
-app.set('port', process.env.PORT || 3000)
-app.locals.title = 'Garage Bin'
+const enviroment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[enviroment];
+const database = require('knex')(configuration);
 
-app.get('/', (request, response) => {
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Garage Bin';
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('lib'));
+
+app.get('/', (request, response ) => {
   fs.readFile(`${__dirname}/index.html`, (err, file) => {
     response.send(file)
   })
+});
+
+//get all items in garage
+app.get('/api/v1/items', (request, response) => {
+  database('items').select()
+  .then(items => {
+    response.status(200).json(items);
+  })
+  .catch(error => {
+    response.sendStatus(500).send({ error });
+  });
 })
+
+// get a single item in the garage
+
+app.get('/api/v1/items/:id', (request, response) => {
+  const { id } = request.params
+  database('items').select().where('id', id)
+    .then((items) => {
+      if (!items.length) {
+        response.status(404).send({ error: 'No items!' });
+      } else {
+        response.status(200).json(items)
+      }
+    })
+    .catch((error) => {
+      response.status(500).send({ error });
+    });
+});
+
+
+// add a new thing to garage
+
+app.post('/api/v1/items', (request, response) => {
+    const correctBody = [ 'name', 'whyItStays', 'cleanliness' ].every(param => request.body[param]);
+    const { name, whyItStays, cleanliness } = request.body;
+
+    if (!correctBody) {
+      return response.status(422).send({ error: 'You are missing content in your request' });
+    }
+    database('items').insert({ name, whyItStays, cleanliness }, [ 'id', 'name', 'whyItStays', 'cleanliness' ])
+      .then((addedItems) => {
+        response.status(200).send(addedItems[0]);
+      })
+      .catch((error) => {
+        response.status(500).send({ error });
+      });
+});
+
+// update the cleanliness by passing in same entry of an item in the garage
+
+app.put('/api/v1/items/:id/edit', (request, response) => {
+  const { id } = request.params;
+  const { cleanliness } = request.body;
+
+  if (!cleanliness) {
+    return response.status(422).send('Sorry you did not pass in the correct info')
+  }
+  database('items').where('id', id).update({ cleanliness: cleanliness })
+    .then(() => {
+      database('items').where('id', id).select()
+      .then((items) => {
+        response.status(200).json(items);
+      })
+      .catch((error) => {
+        response.status(500).send({ error })
+      })
+    });
+})
+
+
+
 
 
 
