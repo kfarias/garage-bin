87426e655c1e74d1730939da59cc1d73be30c35a
Kenia Farias <11431kenia@gmail.com>
2017-06-01 09:45:11 -0600

    more set up and some html text

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const md5 = require('md5')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+app.set('port', process.env.PORT || 3000)
+app.locals.title = 'Garage Bin'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`)
+})
+
+module.exports = app
