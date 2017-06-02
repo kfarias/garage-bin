f360a010eda710797949839a91c8951b52b081e5
Kenia Farias <11431kenia@gmail.com>
2017-06-01 23:30:58 -0600

    fixes for deployment

--- knexfile.js
+++ knexfile.js
@@ -3,23 +3,23 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/items',
-    useNullAsDefault: true,
     migrations: {
-      directory: './db/migrations'
+      directory: './db/migrations',
     },
     seeds: {
       directory: './db/seeds/dev'
-    }
+    },
+    useNullAsDefault: true,
   },
   test: {
     client: 'pg',
     connection: process.env.DATABASE_URL || 'postgres://localhost/testitems',
     migrations: {
-      directory: __dirname + '/db/migrations'
+      directory: '/db/migrations'
     },
     seeds: {
-      directory: __dirname + '/db/seeds/test'
+      directory: '/db/seeds/test'
     },
-    useNullAsDefault: true
-  }
+    useNullAsDefault: true,
+  },
 };
