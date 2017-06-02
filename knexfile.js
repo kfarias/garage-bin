f90f6f45b01954df10d7f38579d7bae518ca35ef
Kenia Farias <11431kenia@gmail.com>
2017-06-02 09:12:06 -0600

    added deployment to knexfile

--- knexfile.js
+++ knexfile.js
@@ -11,6 +11,17 @@ module.exports = {
     },
     useNullAsDefault: true,
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || '?ssl=true',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    }
+  },
   test: {
     client: 'pg',
     connection: process.env.DATABASE_URL || 'postgres://localhost/testitems',
